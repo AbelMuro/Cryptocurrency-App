0fa4c4464443339a2852dd7af0e248b6c4c6e53b
abelmuro <abelmuro93@gmail.com>
2023-01-25 14:02:23 -0800

    commit

--- src/components/App/CryptoCoins/CryptoCoins.js
+++ src/components/App/CryptoCoins/CryptoCoins.js
@@ -5,7 +5,6 @@ import "./styles.css";
 function CryptoCoins({currentPage, getTotalPages}) {
     const [coinData, setCoinData] = useState([]);
     const postsPerPage = useRef(8);
-    console.log("render")
 
     useEffect(() => {
         fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
@@ -17,6 +16,28 @@ function CryptoCoins({currentPage, getTotalPages}) {
                 getTotalPages(Math.round(results.length / postsPerPage.current)) ;
             })
     }, []);
+
+
+        //find out about cURL
+
+        //authorization key:  GKbxxwHiNpjy9UkbdQOGbiAgV0SCX4gHAQ6NUC3P
+        //secret: iMZ1l1ZH1LJS0jVNq1u6PpY7GFZZXYtk0ErYi8bBhgZ8fo9JSq4GaHT3QpXjIdP6
+    useEffect(() => {
+        fetch("https://api.printful.com/store/products", {
+            headers: {
+                Authorization: "Bearer GKbxxwHiNpjy9UkbdQOGbiAgV0SCX4gHAQ6NUC3P"
+              }
+        })
+            .then((response) => {
+                console.log("hehe", response);
+                return response.json()
+            })
+            .then((results) => {
+                console.log(results);
+            })
+
+    })
+
 
     const lastPostIndex = currentPage * postsPerPage.current;
     const firstPostIndex = lastPostIndex - postsPerPage.current;
