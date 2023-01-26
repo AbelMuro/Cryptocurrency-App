2cac7358b443e9b79f611203322e681c38264461
abelmuro <abelmuro93@gmail.com>
2023-01-25 18:25:38 -0800

    commit

--- src/components/App/CryptoCoins/CryptoCoins.js
+++ src/components/App/CryptoCoins/CryptoCoins.js
@@ -18,26 +18,6 @@ function CryptoCoins({currentPage, getTotalPages}) {
     }, []);
 
 
-        //find out about cURL
-
-        //authorization key:  GKbxxwHiNpjy9UkbdQOGbiAgV0SCX4gHAQ6NUC3P
-        //secret: iMZ1l1ZH1LJS0jVNq1u6PpY7GFZZXYtk0ErYi8bBhgZ8fo9JSq4GaHT3QpXjIdP6
-    useEffect(() => {
-        fetch("https://api.printful.com/store/products", {
-            headers: {
-                Authorization: "Bearer GKbxxwHiNpjy9UkbdQOGbiAgV0SCX4gHAQ6NUC3P"
-              }
-        })
-            .then((response) => {
-                console.log("hehe", response);
-                return response.json()
-            })
-            .then((results) => {
-                console.log(results);
-            })
-
-    })
-
 
     const lastPostIndex = currentPage * postsPerPage.current;
     const firstPostIndex = lastPostIndex - postsPerPage.current;
